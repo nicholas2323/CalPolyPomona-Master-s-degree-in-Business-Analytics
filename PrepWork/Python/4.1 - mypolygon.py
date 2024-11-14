74c327de5654f93a443763ffb6e1cf71a123fbb8
Nicholas Petitt <119457260+nicholas2323@users.noreply.github.com>
2024-11-14 13:30:16 -0800

    Prep Work, Chapter 4.1 and Chapter 4.2

---
+++ PrepWork/Python/4.1 - mypolygon.py
@@ -0,0 +1,10 @@
+Create a file named mypolygon.py and type in the following code:
+import turtle
+bob = turtle.Turtle()
+print(bob)
+turtle.mainloop()
+
+Output: 
+    <turtle.Turtle object at 0xb7bfbf4c>
+    
+    
